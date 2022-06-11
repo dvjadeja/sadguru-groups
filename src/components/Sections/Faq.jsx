64ce86a22af21a4a7ccd080e3807588d2ca2a6e4
Dilipsinh Jadeja <dilipsinh.j@unomok.com>
2022-06-11 12:06:15 +0530

    Added Faq and Footer

--- src/components/Sections/Faq.jsx
+++ src/components/Sections/Faq.jsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import React, { useLayoutEffect, useRef } from "react";
+import styled from "styled-components";
+import Accordian from "../Accordian";
+
+const Section = styled.section`
+  min-height: 100vh;
+  width: 100vw;
+  background-color: ${(props) => props.theme.text};
+  position: relative;
+  color: ${(props) => props.theme.body};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  font-size: ${(props) => props.theme.fontxxl};
+  text-transform: uppercase;
+  color: ${(props) => props.theme.body};
+  margin: 1rem auto;
+  border-bottom: 2px solid ${(props) => props.theme.body};
+  width: fit-content;
+`;
+
+const Container = styled.div`
+  width: 75%;
+  margin: 2rem auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Box = styled.div`
+  width: 45%;
+`;
 
 const Faq = () => {
-  return (
-    <div>Faq</div>
-  )
-}
+  const ref = useRef(null);
+  gsap.registerPlugin(ScrollTrigger);
 
-export default Faq
+  useLayoutEffect(() => {
+    let element = ref.current;
+
+    ScrollTrigger.create({
+      trigger: element,
+      start: "top top",
+      end: "bottom top",
+      pin: true,
+      pinSpacing: false,
+      scrub: true,
+    });
+
+    return () => {
+      ScrollTrigger.kill();
+    };
+  }, []);
+
+  return (
+    <Section ref={ref}>
+      <Title>Faq</Title>
+      <Container>
+        <Box>
+          <Accordian
+            title="WHERE CAN I VIEW MY NFTS?"
+            children="Once minted or bought simply connect to your OpenSea account to view your NFTs."
+          />
+          <Accordian
+            title="WHAT IS THE METAVERSE?"
+            children="A metaverse is a network of 3D virtual worlds focused on social connection. In futurism and science fiction, it is often described as a hypothetical iteration of the Internet as a single, universal virtual world that is facilitated by the use of virtual and augmented reality headsets."
+          />
+          <Accordian
+            title="WHY DO WE NEED ROYALTIES?"
+            children="The amount of royalties was fixed at 5% to finance the Weirdos Club's projects. We have the ambition to organize multiple events around the world in order to strengthen the community and build a network of entrepreneurs and investors with the same mindset and common interests."
+          />
+        </Box>
+        <Box>
+          <Accordian
+            title="HOW CAN I USE MY NFT?"
+            children="You will be able to use your NFT as an avatar in the Metaverse and our future video game. Holding also means that you are part of an exclusive network of investors and entrepreneurs."
+          />
+          <Accordian
+            title="WHAT ARE THE WEIRDOS?"
+            children="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore deserunt consequatur quisquam maxime molestias dolores ipsum, exercitationem vel sint quidem aliquam modi quis impedit corporis unde inventore fugiat provident in."
+          />
+          <Accordian
+            title="WHAT IS THE FUSION PROCESS?"
+            children="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel enim veritatis iusto officia. Exercitationem, ducimus reiciendis. Rem, maxime, similique neque minus aliquam dolore doloremque laboriosam, facilis quibusdam unde sint officia."
+          />
+        </Box>
+      </Container>
+    </Section>
+  );
+};
+
+export default Faq;
